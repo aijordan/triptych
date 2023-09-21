8b6b4a109877392d65341cc4da98413b0a352e2e
aijordan <aijordan@users.noreply.github.com>
2023-09-21 12:41:56 +0200

    include a simulated example data set

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Example data set with binary observations and forecasts from nested information sets
+#' 
+#' A more detailed explanation of the simulation setting...
+#' 
+#' @format ## `ex_binary_nested`
+#' A data frame with 1,000 rows and 4 columns:
+#' \describe{
+#'   \item{y}{observations}
+#'   \item{X0}{forecasts from largest information set}
+#'   \item{X1}{forecasts from second largest information set}
+#'   \item{X2}{forecasts from third largest information set}
+#'   \item{X3}{forecasts from smallest information set}
+#' }
+"ex_binary_nested"
